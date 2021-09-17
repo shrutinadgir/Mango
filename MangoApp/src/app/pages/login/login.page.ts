7db56332ac38b65e83f71ca2e160208e07e001c6
bhavya kadali <bhavya.kadali@trianz.com>
2021-09-17 11:34:40 +0530

    login page UI && main route changed to login page

--- MangoApp/src/app/pages/login/login.page.ts
+++ MangoApp/src/app/pages/login/login.page.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
+  formData={email:'',password:''}
   constructor() { }
 
   ngOnInit() {
   }
-
+  login(){
+    console.log("form Data",this.formData)
+  }
 }
