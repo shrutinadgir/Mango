b661ef0d5637170ad1718b8efe6969f6cd91d6a2
Shanu Sulabh <kumarshanu.sulabh@trianz.com>
2021-09-23 13:54:55 +0530

    implemented the structure for calling backend api

---
+++ MangoApp/src/app/services/backend-call/backend-call.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { ApiUrls } from 'src/app/utilities/api-urls';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackendCallService {
+
+  constructor(public http: HttpClient) { }
+
+  getProjectLists() {
+    const _self = this;
+    let urlParams = new HttpParams();
+    return _self.http.get<any>(`${ApiUrls.getProjects}`, { params: urlParams })
+      .toPromise()
+  }
+}
---
+++ MangoApp/src/app/utilities/api-urls.ts
@@ -0,0 +1,5 @@
+import { environment } from 'src/environments/environment';
+
+export class ApiUrls {
+    public static getProjects = environment.baseUrl + '/projects';
+}
