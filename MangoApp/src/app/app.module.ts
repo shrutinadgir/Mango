730743abe1a144f8b1c8dee6d1b9aabc8c8f42df
bhavya kadali <bhavya.kadali@trianz.com>
2021-09-27 21:00:29 +0530

    MSAL Auth login implementation

--- MangoApp/src/app/app.module.ts
+++ MangoApp/src/app/app.module.ts
@@ -9,15 +9,63 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BackendCallService } from './services/backend-call/backend-call.service';
 
+import {
+  MsalModule,
+  MsalRedirectComponent,
+  MsalGuard,
+  MsalInterceptor,
+} from '@azure/msal-angular';
+import { PublicClientApplication, InteractionType } from '@azure/msal-browser';
+
+const isIE =
+  window.navigator.userAgent.indexOf('MSIE ') > -1 ||
+  window.navigator.userAgent.indexOf('Trident/') > -1;
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    MsalModule.forRoot(
+      new PublicClientApplication({
+        auth: {
+          clientId: 'c1d291ce-3126-4892-a00c-af92e9392d24', // This is your client ID
+          authority: 'https://login.microsoftonline.com/organizations', // This is your tenant ID
+          redirectUri: 'http://localhost:8100', // This is your redirect URI
+          // postLogoutRedirectUri:'http://localhost:8100/logout',
+          // navigateToLoginRequestUrl: true,
+        },
+        cache: {
+          cacheLocation: 'localStorage',
+          storeAuthStateInCookie: isIE, // Set to true for Internet Explorer 11
+        },
+      }),
+      {
+        interactionType: InteractionType.Redirect,
+        authRequest: {
+          scopes: ['user.read'],
+        },
+      },
+      {
+        interactionType: InteractionType.Popup,
+        protectedResourceMap: new Map([
+          ['https://graph.microsoft.com/v1.0/me', ['user.read']],
+        ]),
+      }
+    ),
+    HttpClientModule,
+  ],
   providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true,
+    },
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-    BackendCallService
+    BackendCallService,
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- MangoApp/src/app/pages/login/login.page.ts
+++ MangoApp/src/app/pages/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { MsalService } from '@azure/msal-angular';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,11 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
   formData={email:'',password:''}
-  constructor() { }
+  loginDisplay = false;
+  userData: any;
+  constructor(private router: Router,private authService: MsalService) { }
 
   ngOnInit() {
   }
   login(){
     console.log("form Data",this.formData)
+    this.router.navigate(['/dashboard'])
+  }
+
+  loginWithSSO() {
+    this.authService.loginPopup().subscribe({ 
+      next: (result) => {
+        console.log(result, JSON.stringify(result));
+        this.userData = result.account;
+        this.setLoginDisplay();
+      },
+      error: (error) => console.log(error),
+    });
+
+  }
+
+  setLoginDisplay() {
+    this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
+    console.log('setLoginDisplay', this.loginDisplay, this.userData.name);
+    let silentRequest = {
+      scopes:['Mail.Read'],
+      account:this.userData.username,
+      forceRefresh:false
+    }
+    console.log('silentRequest',silentRequest);
+
+    this.authService.acquireTokenSilent(silentRequest).subscribe((res:any)=>{
+      console.log('res',res);
+    },err=>console.log('error',err)
+    )
+    this.router.navigate(['/dashboard'])
+    // this.router.navigate(['/dashboard'], {
+    //   queryParams: { userName: this.userData.name }
+    // });
   }
 }
