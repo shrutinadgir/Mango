0902e1ae630bdf24d99599e72b0c6e1ad93e33c0
bhavya kadali <bhavya.kadali@trianz.com>
2021-09-30 12:35:32 +0530

    sorting pipe added and implemented in dasboard page

--- MangoApp/src/app/pages/dashboard/dashboard.page.ts
+++ MangoApp/src/app/pages/dashboard/dashboard.page.ts
@@ -4,7 +4,7 @@ import { AlertModalPage } from '../alert-modal/alert-modal.page';
 import { BackendCallService } from 'src/app/services/backend-call/backend-call.service';
 import { LoadingController } from '@ionic/angular';
 import { isNotEmptyArray, toNumber } from 'src/app/utilities/utils';
-
+import { SortingPipe } from 'src/app/pipes/sorting.pipe';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
@@ -13,9 +13,13 @@ import { isNotEmptyArray, toNumber } from 'src/app/utilities/utils';
 export class DashboardPage implements OnInit {
   selectedTabTitle: string = 'DashBoard';
   projectLists: Array<[]> = [];
-
-  constructor(public modalController: ModalController, public backendService: BackendCallService,
-    public loadingController: LoadingController) { }
+  sorOrderStatus: boolean = false;
+  constructor(
+    public modalController: ModalController,
+    public backendService: BackendCallService,
+    public loadingController: LoadingController,
+    public sortPipe: SortingPipe
+  ) {}
 
   ngOnInit() {
     this.getProjects();
@@ -23,12 +27,14 @@ export class DashboardPage implements OnInit {
 
   sideNavSelectedTab(tab) {
     const _self = this;
-    if (tab === 'dash') _self.selectedTabTitle = 'DashBoard'
-    else if (tab === 'pm') _self.selectedTabTitle = 'Project Dashboard'
-    else if (tab === 'ofm') _self.selectedTabTitle = 'Offer - Management Dashboard'
-    else if (tab === 'om') _self.selectedTabTitle = 'Order - Management Dashboard'
-    else if (tab === 'um') _self.selectedTabTitle = 'User Management Console'
-    else if (tab === 'lm') _self.selectedTabTitle = 'Library Management'
+    if (tab === 'dash') _self.selectedTabTitle = 'DashBoard';
+    else if (tab === 'pm') _self.selectedTabTitle = 'Project Dashboard';
+    else if (tab === 'ofm')
+      _self.selectedTabTitle = 'Offer - Management Dashboard';
+    else if (tab === 'om')
+      _self.selectedTabTitle = 'Order - Management Dashboard';
+    else if (tab === 'um') _self.selectedTabTitle = 'User Management Console';
+    else if (tab === 'lm') _self.selectedTabTitle = 'Library Management';
   }
 
   async openAlertModal() {
@@ -37,31 +43,43 @@ export class DashboardPage implements OnInit {
       cssClass: 'mango-alert-modal',
       backdropDismiss: false,
       componentProps: {
-        'alertContent': `Are You Sure You Want to Reset to default widgets?`,
-      }
+        alertContent: `Are You Sure You Want to Reset to default widgets?`,
+      },
     });
     return await modal.present();
   }
 
   getProjects() {
     const _self = this;
-    _self.backendService.getProjectLists().then(data => {
-      const { projects } = data;
-      if (isNotEmptyArray(projects)) {
-        projects.forEach(_p => {
-          if (isNaN(_p.Cost_dollars)) _p.Cost_dollars = _self.convertStringToNum(_p.Cost_dollars);
-          if (isNaN(_p.Revenue_dollars)) _p.Revenue_dollars = _self.convertStringToNum(_p.Revenue_dollars);
-          if (isNaN(_p.Time_hours)) _p.Time_hours = _self.convertStringToNum(_p.Time_hours);
-        });
-        return _self.projectLists = projects;
-      }
-    }).catch(err => {
-      console.log("getting error for fetching projects lists:", err);
-    })
+    _self.backendService
+      .getProjectLists()
+      .then((data) => {
+        const { projects } = data;
+        if (isNotEmptyArray(projects)) {
+          projects.forEach((_p) => {
+            if (isNaN(_p.Cost_dollars))
+              _p.Cost_dollars = _self.convertStringToNum(_p.Cost_dollars);
+            if (isNaN(_p.Revenue_dollars))
+              _p.Revenue_dollars = _self.convertStringToNum(_p.Revenue_dollars);
+            if (isNaN(_p.Time_hours))
+              _p.Time_hours = _self.convertStringToNum(_p.Time_hours);
+          });
+          return (_self.projectLists = projects);
+        }
+      })
+      .catch((err) => {
+        console.log('getting error for fetching projects lists:', err);
+      });
   }
   convertStringToNum(value) {
-    let num = value.split("%");
+    let num = value.split('%');
     return toNumber(num[0]);
   }
 
+  sortData(data) {
+    this.sorOrderStatus = !this.sorOrderStatus;
+    this.sorOrderStatus == true
+      ? this.sortPipe.transform(data, 'project_name', 'asc')
+      : this.sortPipe.transform(data, 'project_name', 'dsc');
+  }
 }
