607e49571e988535eeefca981e84bd3ab551d9e4
Shanu Sulabh <kumarshanu.sulabh@trianz.com>
2021-10-06 16:22:38 +0530

    changes the api url for fetching the project data

--- MangoApp/src/app/utilities/api-urls.ts
+++ MangoApp/src/app/utilities/api-urls.ts
@@ -1,5 +1,5 @@
 import { environment } from 'src/environments/environment';
 
 export class ApiUrls {
-    public static getProjects = environment.baseUrl + '/projects';
+    public static getProjects = environment.baseUrl + '/demofe/projects';
 }
